c65e1ee25210cb2fe51f045293b9271a29fdf6a4
AdaptCharm <jamie@jamie.no>
2021-11-06 22:16:46 +0100

    Replace react with preact for production builds

--- next.config.js
+++ next.config.js
@@ -6,4 +6,15 @@ module.exports = {
   images: {
     formats: ['image/avif', 'image/webp'],
   },
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      })
+    }
+
+    return config
+  },
 }
